c100a515970b5213b6878474b0574467e03c8801
JoshPham <JTPham911@gmail.com>
2023-08-30 04:53:03 +0000

    added authentication for devise frontend componenents

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Register</h2>
+    <form @submit="registerUser">
+      <input v-model="email" type="email" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async registerUser(event) {
+      event.preventDefault();
+      try {
+        const response = await axios.post('/api/users', {
+          user: {
+            email: this.email,
+            password: this.password,
+          },
+        });
+        console.log(response.data); // Handle success or redirect
+      } catch (error) {
+        console.error(error.response.data); // Handle error
+      }
+    },
+  },
+};
+</script>
